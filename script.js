b33553c96a32b9c6b6cc3cc7deeb20d33bf24305
Mayuri <mayurisp2052@gmail.com>
2024-08-20 00:18:26 +0530

    file changes

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//change mode
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('theme-toggle');
+
+    themeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-theme');
+
+       
+        if (document.body.classList.contains('dark-theme')) {
+            themeToggle.classList.remove('fa-sun');
+            themeToggle.classList.add('fa-moon');
+        } else {
+            themeToggle.classList.remove('fa-moon');
+            themeToggle.classList.add('fa-sun');
+        }
+    });
+});
 // Check if user information is already stored in local storage
 const storedUserInfo = localStorage.getItem("userInformation");
 
